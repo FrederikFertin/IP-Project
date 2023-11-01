cb86c7e53b12264cb0007fa82fd487ffc7a1e46b
FrederikFertin <s203679@student.dtu.dk>
2023-11-01 22:08:47 +0100

    question14 cleanup

--- question14.jl
+++ question14.jl
@@ -1,40 +1,43 @@
 using JuMP, GLPK
 
-
 # Number of assignments in the matrix
-W= 21
+W = 21
 
-# Number of assignments in the Solution
+# Number of assignments/teams in the solution
 k = 3
 
 # Number of tasks in the problem
 t = 6
 
 # Matrix is column by column,  so first index is column and second index is row
-D =[1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+D = [1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
     0 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0;
     0 0 1 1 1 1 0 1 1 1 1 1 1 1 1 0 0 0 0 0 0; 
     0 0 0 1 1 1 0 0 1 1 1 0 1 1 1 1 1 1 0 0 0; 
     0 0 0 0 1 1 0 0 0 1 1 0 0 1 1 0 1 1 1 1 0;
     0 0 0 0 0 1 0 0 0 0 1 0 0 0 1 0 0 1 0 1 1]
 
-s = [100 200 400 500 900 700]
-
 # Cost vector
 c = [100 300 700 1200 2100 2800 200 600 1100 2000 2700 400 900 1800 2500 500 1400 2100 900 1600 700]
 
+# Initialise model
 model = Model(GLPK.Optimizer)
 
+# Decision variables
 @variable(model, x[1:W], Bin)
 @variable(model, Z)
 
-@objective(model, Min, Z)
-
+# Each task should be done exactly once
 @constraint(model, [j=1:t], sum(x[i]*D[j,i] for i=1:W) == 1)
 
+# The number of assignments chosen should be equal to the number of teams
 @constraint(model, sum(x) == k)
 
+# Z is larger than all team costs
 @constraint(model, [i=1:W], Z >= x[i]*c[i])
+
+# Objective function minimizing the maximum assignment cost
+@objective(model, Min, Z)
 
 JuMP.optimize!(model)
 
